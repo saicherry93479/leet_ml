98d30f279f083270fd94bf27c9d4321222d01f51
sthipp005 <saicherry93479@gmail.com>
2024-11-09 17:16:19 +0530

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,12 @@ import svelte from "@astrojs/svelte";
 
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  adapter: deno({
+    port: 4321,
+    hostname: "0.0.0.0",
+  }),
   output: "server",
   experimental: {
     serverIslands: true,
